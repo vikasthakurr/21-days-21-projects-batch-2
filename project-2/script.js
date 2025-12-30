249e827ee1fcde8d17b2a5a52e26b444c105b599
vikasthakurr <vikaskumar20012001@gmail.com>
2025-12-30 22:20:28 +0530

    text code

--- project-2/script.js
+++ project-2/script.js
@@ -8,3 +8,40 @@ toggleBtn.addEventListener("click", () => {
   const isLight = document.body.classList.contains("light-theme");
   toggleBtn.textContent = isLight ? "🌙" : "🌞";
 });
+
+const typedTextSpan = document.querySelector(".typed-text");
+const textArray = ["Software Engineer", "Mentor", "Comedian", "Runner"];
+const typingDelay = 100;
+const erasingDelay = 50;
+const newTextDelay = 2000;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(
+      0,
+      charIndex - 1
+    );
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    textArrayIndex++;
+    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+    setTimeout(type, typingDelay);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  if (textArray.length) setTimeout(type, newTextDelay);
+});
